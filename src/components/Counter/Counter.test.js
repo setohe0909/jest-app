212146ee7e15daef3dad98ff16486fd8563d2f9f
sebastian tobon <setohe.09@gmail.com>
2020-01-29 09:37:00 -0500

    test on hooks

---
+++ src/components/Counter/Counter.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
+import Counter from './Counter'
+
+configure({
+  adapter: new Adapter()
+})
+
+describe('Probando componente <Counter />', () => {
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<Counter />)
+  })
+
+  test('Validar que coincida con Snapshot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  test('Validar funcionamiento de botones', () => {
+
+    wrapper.find('button').first().simulate('click')
+
+    expect(wrapper.find('h1').text()).toBe('1')
+
+    wrapper.find('button').last().simulate('click')
+    wrapper.find('button').last().simulate('click')
+    wrapper.find('button').last().simulate('click')
+
+    expect(wrapper.find('h1').text()).toBe('-2')
+
+  })
+
+})
